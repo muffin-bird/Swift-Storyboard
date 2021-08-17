02e9aca7a211d168e16b7487fd4f990afd737cf2
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-18 01:19:34 +0900

    Create Mypractice2.storyboard

---
+++ chapter_4/MyPractice2.playground/Contents.swift
@@ -0,0 +1,25 @@
+// 文字列
+print("Hello World Swift!")
+
+// 文字列を変数に代入
+var alert = "Hello World Swift!"
+print(alert)
+
+//文字列に変数を埋め込む (文字列補間)
+var battery = 18
+print("バッテリー残量はあと\(battery)%です")
+
+//ブール値
+print(1 == 1)
+print(2 == 1)
+
+//※変数には同じ型の値しか入らない
+
+// 変数宣言時に型を指定する
+var number:Int = 3
+let pi:Double = 3.14
+
+//Practice 4-1
+var bodyTemp:Double = 36.0
+bodyTemp = 36.5
+
