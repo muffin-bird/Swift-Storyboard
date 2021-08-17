b3c244a086795f4d95a5ec0e194596fc53c8bf20
muffin-bird <muffinmen617@gmail.com>
2021-08-17 19:09:51 +0900

    Add Mypractice.storyboard

--- chapter_3/MyPractice.playground/Contents.swift
+++ chapter_3/MyPractice.playground/Contents.swift
@@ -3,3 +3,21 @@ print(5 - 3)
 print(3 * 3)
 print(6 / 2)
 print(7 % 3)
+
+var y = 3
+for n in 1...9 {
+    print(y * n)
+}
+
+// Practice 3-1
+var x = 2
+print(x * x)
+
+// Practice 3-2
+var sum = 0
+for m in 1...100 {
+    sum = sum + m
+}
+print(sum)
+
+
