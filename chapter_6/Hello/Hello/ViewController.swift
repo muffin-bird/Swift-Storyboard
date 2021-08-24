836fa7ab5596d5d7fb00755d2ffb35ad88f5e44e
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-24 13:50:43 +0900

    Viewcontroller to Link

--- chapter_6/Hello/Hello/ViewController.swift
+++ chapter_6/Hello/Hello/ViewController.swift
@@ -4,12 +4,15 @@
 //
 //  Created by muffin man on 2021/08/22.
 //
-// testgithub
+//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBAction func sayHello() {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
