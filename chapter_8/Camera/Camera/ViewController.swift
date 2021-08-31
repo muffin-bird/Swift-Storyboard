ff8d1ca32373a52006e710579dd4c846eaa501c1
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-01 03:15:28 +0900

    Create CameraApp

---
+++ chapter_8/Camera/Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by muffin man on 2021/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
