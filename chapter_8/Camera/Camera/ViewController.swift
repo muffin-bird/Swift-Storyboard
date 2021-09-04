b0b212c7cc30242b1924db9c8b82c00a7f6808b7
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-04 14:45:16 +0900

    Add Objects

--- chapter_8/Camera/Camera/ViewController.swift
+++ chapter_8/Camera/Camera/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
