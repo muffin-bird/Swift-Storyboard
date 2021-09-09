5cf3b71afeabba15e6560c9e5618ec503be75244
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-09 17:13:09 +0900

    AutoLayout Change

--- chapter_8/Camera/Camera/ViewController.swift
+++ chapter_8/Camera/Camera/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+        
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
         
         // カメラがタップされた時の処理
-        // 画像の取得方法を定数に代入 (列拳体)
+        // 画像の取得方法を定数に代入
         let camera = UIImagePickerController.SourceType.camera
         
         // デバイスチェック
@@ -26,17 +26,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     // 撮影完了時の処理
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         self.imageView.image = image
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
         self.dismiss(animated: true)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
