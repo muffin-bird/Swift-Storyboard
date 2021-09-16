9d19fbc8556c7c342bf64451c61f840221ff0d97
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-16 19:26:46 +0900

    Multiple  Protocol

--- chapter_8/Protocol.playground/Contents.swift
+++ chapter_8/Protocol.playground/Contents.swift
@@ -1,8 +1,13 @@
 import UIKit
+
 // プロトコルの宣言
 @objc protocol KyoutoProtocol {
     func stopGlobalWarming()
     @objc optional func chargeCarbonTax()
+}
+
+protocol WashingtonConvention {
+    func stopExtinctionAnimal()
 }
 
 // 親クラス
@@ -10,10 +15,13 @@ class Asia {
     
 }
 // クラス
-class Japan:Asia, KyoutoProtocol {
+class Japan:Asia, KyoutoProtocol, WashingtonConvention {
     func stopGlobalWarming() {
         print("クリーンエネルギーを推進します。")
         print("森林を増やします。")
+    }
+    func stopExtinctionAnimal() {
+        print("絶滅動物を保護します。")
     }
 }
 
